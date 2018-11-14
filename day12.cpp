091448667cd891c1b1786fe38ac0906d8148231c
Ravie403 <info@tkm.blue>
2018-11-14 17:06:11 +0900

    sample

--- day12.cpp
+++ day12.cpp
@@ -7,6 +7,14 @@ using namespace std;
 void Powering(int& x){
   x*=x;
 }
+int Modulus(int x,int y,int& m){
+  if(y==0){
+    m = 0;
+    return 0;
+  }
+   m = x%y;
+   return x/y;
+}
 int main(){
   int a = 5;
   cout << a << endl;
@@ -14,5 +22,8 @@ int main(){
   cout << a << endl;
   Powering(a);
   cout << a << endl;
+  int x = 10,y = 4,r;
+  int d = Modulus(x,y,r);
+  cout << d <<" "<<r << endl;
   return 0;
 }
