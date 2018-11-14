d84e8e5f16857a1836e329832f4aabe5f917f350
Ravie403 <info@tkm.blue>
2018-11-14 17:15:47 +0900

    day14

---
+++ Makefile
@@ -0,0 +1,2 @@
+Compile14: day14.cpp day14.hpp
+	g++ day14.cpp -std=c++0x -O2
---
+++ day14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "day14.hpp"
+using namespace std;
+
+int main(){
+  cppfunc();
+  return 0;
+}
+
+void cppfunc(){
+  cout << "OK" << endl;
+}
